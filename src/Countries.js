9646f88c7bb68f23a658375ff8503edf5b175d83
dilip <dilip.shital@gmail.com>
2020-07-14 12:56:18 +0530

    covid app

---
+++ src/Countries.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Countries extends Component {
+    render() {
+        const {countries}= this.props;
+        return (
+            <tr>
+                <td>{countries.Country}</td>
+                <td>{countries.NewConfirmed}</td>
+                <td>{countries.TotalRecovered}</td>
+                <td>{countries.TotalDeaths}</td>
+            </tr>
+        )
+    }
+}
