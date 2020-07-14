6875b29e1be6af7b78bc7cbfa025974d033d2654
dilip <dilip.shital@gmail.com>
2020-07-14 13:06:04 +0530

    Updates

---
+++ precache-manifest.372351f80330c911fbb3ef174651bd17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe8a5c15fbac30a93ec986952e8d3de",
+    "url": "/dilip9066/covid_web.git/index.html"
+  },
+  {
+    "revision": "b5c95efaa98a09118822",
+    "url": "/dilip9066/covid_web.git/static/css/main.834e8703.chunk.css"
+  },
+  {
+    "revision": "cc5183be98b26af450b0",
+    "url": "/dilip9066/covid_web.git/static/js/2.0fafb986.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dilip9066/covid_web.git/static/js/2.0fafb986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5c95efaa98a09118822",
+    "url": "/dilip9066/covid_web.git/static/js/main.85569afe.chunk.js"
+  },
+  {
+    "revision": "21bd932a048c4c243a8a",
+    "url": "/dilip9066/covid_web.git/static/js/runtime-main.ff5bfceb.js"
+  }
+]);
